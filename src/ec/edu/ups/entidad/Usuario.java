813f8b688fff2ba341865019a34a5c32c61070ba
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-21 23:42:28 -0500

    Create Usuario.java

---
+++ src/ec/edu/ups/entidad/Usuario.java
@@ -0,0 +1,139 @@
+package ec.edu.ups.entidad;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.*;
+
+
+/**
+ * Entity implementation class for Entity: Usuario
+ *
+ */
+@Entity
+
+public class Usuario implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	@Column(unique = true, nullable = false)
+	private String correo;
+	private String password;
+	@Transient
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+	private Set<Telefono> telefonos;
+
+	public Usuario() {
+		
+	}
+
+	public Usuario(String cedula, String nombre, String apellido, String correo, String password) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+		this.password = password;
+	}
+
+	/*
+	 * GETTERS & SETTERS
+	 *
+	 **/
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(Set<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
+
+	/*
+	 * Para valores unicos, los cuales no se pueden repetir 
+	 * 
+	 * Para la cedula y correo 
+	 **/
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cedula == null) ? 0 : cedula.hashCode());
+		result = prime * result + ((correo == null) ? 0 : correo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (cedula == null) {
+			if (other.cedula != null)
+				return false;
+		} else if (!cedula.equals(other.cedula))
+			return false;
+		if (correo == null) {
+			if (other.correo != null)
+				return false;
+		} else if (!correo.equals(other.correo))
+			return false;
+		return true;
+	}
+
+	// Método toString que permite describir a un objeto
+	@Override
+	public String toString() {
+		return "Usuario [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo
+				+ ", password=" + password + ", telefonos=" + telefonos + "]";
+	}
+   
+}
