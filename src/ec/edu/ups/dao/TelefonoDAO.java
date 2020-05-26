18f315747de25e6b292eabca42ae1df94c3ccc05
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-05-25 19:34:45 -0500

    Create TelefonoDAO.java

---
+++ src/ec/edu/ups/dao/TelefonoDAO.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ec.edu.ups.dao;
+
+
+import java.util.Set;
+
+import ec.edu.ups.entidad.Telefono;
+
+
+/**
+ * @author Orlando Real
+ *
+ */
+public interface TelefonoDAO extends GenericDAO<Telefono, String>{
+
+	/*
+	 * Metodo propio de la interface telefono DAO para obtener un telefono mediante el numero de cedula
+	 * 
+	 * */
+	/*
+	public abstract Set<Telefono> findByUserId(String cedula);
+	
+	public abstract Telefono findbyTelefonoId(int tel_codigo);
+	*/
+}
